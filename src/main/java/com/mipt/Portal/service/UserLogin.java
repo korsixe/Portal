fc00c723114f48dad75741fc44c1d421f207fe46
Доронина Елизавета Андреевна <shuralevakanon@gmail.com>
2025-10-15 19:39:27 +0300

    Update UserLogin.java

--- src/main/java/com/mipt/Portal/service/UserLogin.java
+++ src/main/java/com/mipt/Portal/service/UserLogin.java
@@ -2,4 +2,8 @@ package com.mipt.Portal.service;
 
 public interface UserLogin {
   User login(String email, String password);
+  
+  boolean userExists(String email);
+
+  boolean passwordCorrect(String password);
 }
