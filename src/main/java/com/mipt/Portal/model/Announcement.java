a5e20721fecd7796391c4e82820ec3d7a6a25e52
elizaveta_orlova <eko0720@mail.ru>
2025-10-15 15:44:01 +0300

    fixed

---
+++ src/main/java/com/mipt/Portal/model/Announcement.java
@@ -0,0 +1,20 @@
+package com.mipt.Portal.model;
+
+import java.util.Date;
+
+public class Announcement {
+
+  private Long id;
+
+  private String title;
+  private String description;
+  private Double price;
+  private String category;
+  private Date creationDate;
+  private Date updateDate;
+  private String status;
+
+  private User user;
+
+  // Write getters and setters
+}
