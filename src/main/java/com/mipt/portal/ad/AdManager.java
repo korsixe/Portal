9e49728612d154a72fd6b2933b9bf8caa405830d
korsixe <maloytoptop@gmail.com>
2025-10-21 00:20:53 +0300

    Работает, победа

--- src/main/java/com/mipt/portal/ad/AdManager.java
+++ src/main/java/com/mipt/portal/ad/AdManager.java
@@ -4,12 +4,19 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
+import java.io.*;
 
 public class AdManager implements IAdManager {
 
   @Override
   public Ad createAd(String email) {
-    Scanner scanner = new Scanner(System.in);
+    Scanner scanner = null;
+    try {
+      // Устанавливаем правильную кодировку для Scanner
+      scanner = new Scanner(new InputStreamReader(System.in, "UTF-8"));
+    } catch (UnsupportedEncodingException e) {
+      scanner = new Scanner(System.in); // fallback
+    }
 
     System.out.println("Введите заголовок объявления:");
     String title = scanner.nextLine();
@@ -24,13 +31,13 @@ public class AdManager implements IAdManager {
     scanner.nextLine(); // очистка буфера
     Category category = Category.getByNumber(categoryChoice);
 
-    System.out.println("Введите местоположение объявления:"); //Предварительно храним в строках
+    System.out.println("Введите местоположение объявления:");
     String location = scanner.nextLine();
 
-    List<String> chooseCondition = new ArrayList<>(Arrays.asList("б/y", "Новое", "Не работает"));
+    List<String> chooseCondition = new ArrayList<>(Arrays.asList("б/у", "Новое", "Не работает"));
     System.out.println("Введите состояние объявления: ");
     for (int i = 0; i < chooseCondition.size(); ++i) {
-      System.out.println(i + 1 + " " + chooseCondition.get(i));
+      System.out.println((i + 1) + " " + chooseCondition.get(i));
     }
     int type = scanner.nextInt();
 
@@ -38,16 +45,15 @@ public class AdManager implements IAdManager {
       System.out.println("Введеный тип не корректен, попробуйте снова");
       System.out.println("Введите состояние объявления: ");
       for (int i = 0; i < chooseCondition.size(); ++i) {
-        System.out.println(i + 1 + " " + chooseCondition.get(i));
+        System.out.println((i + 1) + " " + chooseCondition.get(i));
       }
-
       type = scanner.nextInt();
     }
     String condition = chooseCondition.get(type - 1);
 
     int price = -1;
     System.out.println("Цена договорная? Введите число: 1 - Да, 2 - Нет");
-    type = scanner.nextInt();                     // Можно поломать, если вводить не число :(
+    type = scanner.nextInt();
     while (type != 1 && type != 2) {
       System.out.println("Вы написали неверное число.\n");
       System.out.println("Цена договорная? Введите число: 1 - Да, 2 - Нет");
@@ -56,7 +62,7 @@ public class AdManager implements IAdManager {
 
     if (type == 2) {
       System.out.println("Введите стоимость товара, цена от 0 до 1000000000");
-      price = scanner.nextInt();                     // Можно поломать, если вводить не число :(
+      price = scanner.nextInt();
       while (price < 0 || price > 1000000000) {
         System.out.println("Введите стоимость товара, цена от 0 до 1000000000");
         price = scanner.nextInt();
@@ -79,14 +85,7 @@ public class AdManager implements IAdManager {
     }
 
     Ad ad = new Ad(title, description, category, condition, price, location, email, status);
-
-    /* добавляем объявление в БД
-    if (type == 1) {
-
-    } else if (type == 2) {
-
-    }
-    */
+    // а тут взяли и в БД добавили
     scanner.close();
     System.out.println("Объявление добавлено!");
     System.out.println(ad.toString());
@@ -96,18 +95,15 @@ public class AdManager implements IAdManager {
 
   @Override
   public Ad editAd(Ad ad) {
-    // Логика редактирования объявления
     return ad;
   }
 
   @Override
   public void deleteAd(int adId) {
-    // Логика удаления объявления
   }
 
   @Override
   public Ad getAd(int adId) {
-    // Логика получения объявления
-    return null; // Вернуть найденное объявление
+    return null;
   }
 }
