54376b54e4c0ad6940798f15864826910120edf6
korsixe <maloytoptop@gmail.com>
2025-11-04 20:12:07 +0300

    + lombok

--- src/main/java/com/mipt/portal/announcement/Announcement.java
+++ src/main/java/com/mipt/portal/announcement/Announcement.java
@@ -6,9 +6,14 @@ import java.time.format.DateTimeFormatter;
 import java.time.ZoneId;
 import java.util.ArrayList;
 import java.util.List;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
+
 public class Announcement {
 
   private long id;                     // id объявления
