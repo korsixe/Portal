32253a719a713daca1625f07c316820f0fe62092
elizaveta_orlova <eko0720@mail.ru>
2025-11-07 20:36:37 +0300

    sql

--- src/main/resources/sql/create_tables.sql
+++ src/main/resources/sql/create_tables.sql
@@ -44,3 +44,15 @@ CREATE TABLE IF NOT EXISTS moderators
     password    TEXT        NOT NULL,
     permissions TEXT[]    DEFAULT '{}'
 );
+
+-- Создание таблицы комментариев
+CREATE TABLE IF NOT EXISTS comments (
+                                        id BIGSERIAL PRIMARY KEY,                    -- Уникальный ID
+                                        ad_id BIGINT NOT NULL,                       -- ID объявления
+                                        user_id BIGINT NOT NULL,                     -- ID пользователя
+                                        content TEXT NOT NULL,                       -- Текст комментария
+                                        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP, -- Дата создания
+
+                                        FOREIGN KEY (ad_id) REFERENCES ads(id) ON DELETE CASCADE,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+    );
