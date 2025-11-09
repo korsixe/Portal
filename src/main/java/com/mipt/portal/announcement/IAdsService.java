5dadc336d90a09584e1663e93751ad44acfebbc3
korsixe <maloytoptop@gmail.com>
2025-11-09 21:26:10 +0300

    funck for manager

--- src/main/java/com/mipt/portal/announcement/IAdsService.java
+++ src/main/java/com/mipt/portal/announcement/IAdsService.java
@@ -11,4 +11,16 @@ public interface IAdsService {
   Announcement deleteAd(long adId); // Удалить объявление
 
   Announcement getAd(long adId); // Получить объявление
+
+  void sendToModeration(Announcement ad) throws SQLException;
+
+  void activate(Announcement ad) throws SQLException;
+
+  void archive(Announcement ad) throws SQLException;
+
+  void saveAsDraft(Announcement ad) throws SQLException;
+
+  void rejectModeration(Announcement ad) throws SQLException;
+
+  void delete(Announcement ad) throws SQLException;
 }
